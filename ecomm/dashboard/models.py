425d8cc9af779b1f52ba0b3d1bcc439e9ae2a4c7
HaRi-Haran-D <hvj58128@gmail.com>
2026-04-06 18:53:44 +0530

    Project Update

--- ecomm/dashboard/models.py
+++ ecomm/dashboard/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product_name = models.CharField(max_length=100)
+    product_price = models.IntegerField()
+    product_describ = models.TextField()
+    product_image = models.ImageField(default='', upload_to='products')
+    product_brand = models.CharField(max_length=100)
+    product_qty = models.IntegerField()
+    created_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.product_name
--- ecomm/dashboard/views.py
+++ ecomm/dashboard/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Product
+from .forms import ProductForm
 
 # Create your views here.
 def home(request):
     return render(request, 'dashboard/home.html')
+
+def add_product(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('dashboard:home')
+    return render(request, 'dashboard/')
